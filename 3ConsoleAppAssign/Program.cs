055cef92cd535c836ab7ff57c9209bfee5d6a7d7
ugonmaforsblom@gmail.com <ugonmaforsblom@gmail.com>
2021-03-12 14:49:47 +0100

    Add project files.

---
+++ 3ConsoleAppAssign/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3ConsoleAppAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
