a0900b7993a0021489e6a74eaaa4875127c76060
ugonmaforsblom@gmail.com <ugonmaforsblom@gmail.com>
2021-03-17 19:46:35 +0100

    add new class PersonSequencer

--- 3ConsoleApp.Test/UnitTest1.cs
+++ 3ConsoleApp.Test/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using _3ConsoleApp.Test.modell;
+using _3ConsoleAppAssign.Data;
 
 namespace _3ConsoleApp.Test
 {
@@ -21,24 +22,31 @@ namespace _3ConsoleApp.Test
             //Assert
             Assert.Equal(description, todo.getDescription());
             Assert.Equal(todoId, todo.getTodId());
-           
 
         }
-        
+        [Fact]
+        public void TestPersonSequencer()
+        {
+            //Arrange
+            int expectedNesxpersonId = 1;
+
+            Assert.Equal(expectedNesxpersonId, PersonSequencer.nextPersonId());
+            PersonSequencer.reset();
+            Assert.Equal(0, PersonSequencer.getPersonId());
+
+            //Assert
 
 
-        
 
-        
-           
+        }
 
-       
+
 
     }
 
 
 
 
-  
+
 
 }
---
+++ 3ConsoleAppAssign/Data/PersonSequencer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3ConsoleAppAssign.Data
+{
+     public class PersonSequencer
+    {
+        //creating fields and methods
+        private static int personId = 0;
+
+       public static int getPersonId ()
+        {
+            return personId;
+        }
+
+        public static int nextPersonId()
+        {
+            
+            personId++; 
+            
+            return personId;
+
+        }
+
+
+        public static void reset()
+
+        {
+            personId = 0;
+
+        }
+
+        
+       
+
+        
+   
+
+     //    
+
+
+
+        }
+        
+    
+}
